ac1f3dbe019a33089c76b52412b994df54ef822d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-12-05 18:51:46 -0500

    Blog updated

---
+++ google-analytics.js
@@ -0,0 +1,7 @@
+(function(i,s,o,g,r,a,m){i[&apos;GoogleAnalyticsObject&apos;]=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,&apos;script&apos;,&apos;//www.google-analytics.com/analytics.js&apos;,&apos;ga&apos;);
+
+ga(&apos;create&apos;, &apos;UA-59999353-1&apos;, &apos;auto&apos;);
+ga(&apos;send&apos;, &apos;pageview&apos;);
